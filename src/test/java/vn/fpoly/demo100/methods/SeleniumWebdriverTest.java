47ada74633cddeed96fbbb856a9d07d196d3e982
Nguyen <emailcuaban@gmail.com>
2026-04-19 22:54:55 +0700

    lennnn

--- src/test/java/vn/fpoly/demo100/methods/SeleniumWebdriverTest.java
+++ src/test/java/vn/fpoly/demo100/methods/SeleniumWebdriverTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class SeleniumWebdriverTest {
-
+/*
 
     WebDriver webDriver;
     @BeforeEach
@@ -53,6 +53,8 @@ webDriver.quit();
     }
 
 
+
+ */
 
 
 }

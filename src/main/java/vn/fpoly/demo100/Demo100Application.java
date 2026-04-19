6e5b4ea1869edcecdf809b76b857797199c0a73c
Nguyen <emailcuaban@gmail.com>
2026-04-19 23:03:01 +0700

    lennnn100all

--- src/main/java/vn/fpoly/demo100/Demo100Application.java
+++ src/main/java/vn/fpoly/demo100/Demo100Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo100Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Demo100Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Demo100Application.class, args);
+    }
 
 }

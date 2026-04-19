5244019fb4ad2772970dac9f93f6a5ae16d721b7
Nguyen <emailcuaban@gmail.com>
2026-04-19 23:00:29 +0700

    lennnn100

--- src/test/java/vn/fpoly/demo100/methods/MyUtilTest.java
+++ src/test/java/vn/fpoly/demo100/methods/MyUtilTest.java
@@ -1,4 +1,23 @@
+package vn.fpoly.demo100.methods;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MyUtilTest {
-  
+MyUtil myUtil = new MyUtil();
+    @Test
+    void tinhtongmang() {
+        assertEquals(100,myUtil.tinhtongmang(new int[]{99,1}));
+    }
+
+    @Test
+    void tinhtongmangwithnull(){
+        Exception exception = assertThrows(IllegalArgumentException.class,()->myUtil.tinhtongmang(null));
+        assertEquals("null",exception.getMessage());
+    }
+    @Test
+    void tinhtongmangphantu0(){
+        assertEquals(0,myUtil.tinhtongmang(new int[]{}));
+    }
 }

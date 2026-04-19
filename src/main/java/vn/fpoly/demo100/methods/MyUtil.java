8009ae31af53f8cbace701ab7d9683ecbf101803
Nguyen <emailcuaban@gmail.com>
2026-04-19 22:56:00 +0700

    lennnn

--- src/main/java/vn/fpoly/demo100/methods/MyUtil.java
+++ src/main/java/vn/fpoly/demo100/methods/MyUtil.java
@@ -1,4 +1,14 @@
 package vn.fpoly.demo100.methods;
 
 public class MyUtil {
+    public int tinhtongmang(int arr[]){
+        if (arr==null){
+            throw new IllegalArgumentException("null");
+        }
+        int tong = 0;
+        for (int i = 0;i<arr.length;i++){
+            tong += arr[i];
+        }
+        return tong;
+    }
 }
